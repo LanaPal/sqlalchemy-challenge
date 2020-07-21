6851c94dbc609ca7df39af8cde35b5156c1cf8b2
LanaPal <64497295+LanaPal@users.noreply.github.com>
2020-07-20 23:00:19 -0500

    changes to the app file

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
+from datetime import datetime, timedelta
 
 from flask import Flask, jsonify
 
@@ -30,27 +32,53 @@ def root():
     return (
         f"Available API routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/station<br/>"
+        f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start><br/>"
         f"//api/v1.0/<start>/<end>")
 
-# @app.route("/api/v1.0/precipitation")
-# def precipitation():
-#     #create session from python to db
-#     session = Session(engine)
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+#create session (link) from python to db
+    session = Session(engine)
+    # Perform a query to retrieve the data and precipitation scores for 12 months period starting from 2016-8-23
+    results = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date > dt.date(2016, 8, 23)).\
+    order_by(Measurement.date.desc()).all()
+    
+    session.close()
+    #convert results into list
+    perc_data = list(np.ravel(results))
+    return jsonify(perc_data)
 
-#     #query
-#     results = session.query(Measurement.date, Measurement.prcp).all()
-#     session.close()
-#     #convert results into list
-#     perc_data = list(np.ravel(results))
-#     return jsonify(perc_data)
+@app.route("/api/v1.0/stations")
+def stations():
+#create session (link) from python to db
+    session = Session(engine)
+    ## List the stations and the counts of weather observations in the descending order.
+    results = session.query(Measurement.station, func.count(Measurement.station)).\
+    group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc()).all()
+    
+    session.close()
+    #convert results into list
+    station_data = list(np.ravel(results))
+    return jsonify(station_data)
 
-# # def stations():
-# # /api/v1.0/stations
-# # /api/v1.0/stations
-# # /api/v1.0/<start> and /api/v1.0/<start>/<end>
+@app.route("/api/v1.0/tobs")
+def tobs():
+#create session (link) from python to db
+    session = Session(engine)
+    ## List the stations and the counts of weather observations in the descending order.
+    results = session.query(Measurement.station, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+    filter(Measurement.station == 'USC00519281').all()
+    
+    session.close()
+    #convert results into list
+    tobs_data = list(np.ravel(results))
+    return jsonify(tobs_data)
+
+# /api/v1.0/<start> and /api/v1.0/<start>/<end>
 
 
 
